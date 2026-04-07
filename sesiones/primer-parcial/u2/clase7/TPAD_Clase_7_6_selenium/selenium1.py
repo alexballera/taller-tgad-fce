ab3aadf231803afbd692d5232041da97a4d88d96
Alex Ballera <alexballera@gmail.com>
2026-04-06 20:22:36 -0400

    feat: update requirements and enhance Selenium scripts for web scraping

--- sesiones/primer-parcial/u2/clase7/TPAD_Clase_7_6_selenium/selenium1.py
+++ sesiones/primer-parcial/u2/clase7/TPAD_Clase_7_6_selenium/selenium1.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # Configurar el navegador
 options_chrome = webdriver.ChromeOptions()
-#options.add_argument("--headless")  # Ejecutar en segundo plano (sin abrir ventana)
-driver = webdriver.Chrome(options=options_chrome)
+#options_chrome.add_argument("--headless")  # Ejecutar en segundo plano (sin abrir ventana)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options_chrome)
 
 driver.get("https://quotes.toscrape.com/scroll")
 
-time.sleep(3)
+time.sleep(120)
 
 driver.quit()
--- sesiones/primer-parcial/u2/clase7/TPAD_Clase_7_6_selenium/selenium3.py
+++ sesiones/primer-parcial/u2/clase7/TPAD_Clase_7_6_selenium/selenium3.py
@@ -1,7 +1,8 @@
+#%%
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-
+#%%
 driver = webdriver.Chrome()
 driver.get("http://quotes.toscrape.com/")
 
@@ -23,3 +24,4 @@ while page<=q_pages:
     page=page+1
 
 driver.quit()
+# %%
